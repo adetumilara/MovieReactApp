1026a594810065bbde051f038388bed315d9f092
adetumilara <adetumilara.adetayo@stu.cu.edu.ng>
2025-01-31 17:25:43 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Item from "./Item";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [sortBy, setSortBy] = useState("input");
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => {
+      return items.filter((item) => item.id !== id);
+    });
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) {
+      setItems([]);
+      setSortBy("input");
+    } else return;
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onClearList={handleClearList}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
